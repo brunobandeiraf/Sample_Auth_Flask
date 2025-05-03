437e9102ca0b63669455173e13aa1e87c7b71ce2
Bruno Bandeira Fernandes <brunobandeirafernandes@gmail.com>
2025-05-03 08:52:52 -0300

    Criando Método de Login

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager, login_user, current_user
 from database import db
-from models.user import User  # Isso agora funciona sem circularidade
+from models.user import User  
+
  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+ #view login
+
+login_manager.login_view = 'login'
  #Session <- conexão ativa
  
+@login_manager.user_loader
+def load_user(user_id):
+   return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
+def login():
+   data = request.json
+   username = data.get("username")
+   password = data.get("password")
+ 
+   if username and password:
+    # Login
+     user = User.query.filter_by(username=username).first()
+ 
+     if user and user.password == password:
+        login_user(user)
+        print(current_user.is_authenticated)
+        return jsonify({"message": "Autenticação realizada com sucesso!"})
+ 
+   return jsonify({"message": "Credenciais inválidas"}), 400
+
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "Hello world"
