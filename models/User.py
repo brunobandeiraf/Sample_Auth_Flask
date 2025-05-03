3b3eba916c1352b4da12c18bb775a6bc0831b1c9
Bruno Bandeira Fernandes <brunobandeirafernandes@gmail.com>
2025-05-03 08:13:07 -0300

    Criando Models User Corretamente

--- models/User.py
+++ models/User.py
@@ -1,6 +1,7 @@
-from app import db
- 
-class User(db.Model):
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
     # id (int), username (text), password (text)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
